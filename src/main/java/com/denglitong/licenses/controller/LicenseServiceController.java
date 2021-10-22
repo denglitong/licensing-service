2a03171fea0d4b9c4b5f0f0f4fdd26c85a1f56b3
denglitong <litong.deng@foxmail.com>
2021-10-22 10:16:23 +0800

    Add REST api endpoint
    Signed-off-by: denglitong <litong.deng@foxmail.com>

--- src/main/java/com/denglitong/licenses/controller/LicenseServiceController.java
+++ src/main/java/com/denglitong/licenses/controller/LicenseServiceController.java
@@ -1,10 +1,10 @@
 package com.denglitong.licenses.controller;
 
 import com.denglitong.licenses.model.License;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.denglitong.licenses.service.LicenseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author litong.deng@foxmail.com
@@ -14,7 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/v1/organizations/{organizationId}/licenses")
 public class LicenseServiceController {
 
-    @GetMapping(value = "/{licenseId}")
+    @Autowired
+    private LicenseService licenseService;
+
+    @GetMapping( "/{licenseId}")
     public License getLicenses(@PathVariable("organizationId") String organizationId,
                                @PathVariable("licenseId") String licenseId) {
         return new License()
@@ -22,5 +25,21 @@ public class LicenseServiceController {
                 .withOrganizationId(organizationId)
                 .withProductName("Teleco")
                 .withLicenseType("Seat");
+    }
+
+    @PutMapping("/{licenseId}")
+    public String updateLicenses(@PathVariable("licenseId") String licenseId) {
+        return String.format("This is the put");
+    }
+
+    @PostMapping("/{licenseId}")
+    public String saveLicenses(@PathVariable("licenseId") String licenseId) {
+        return String.format("This is the post");
+    }
+
+    @DeleteMapping("/{licenseId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public String deleteLicenses(@PathVariable("licenseId") String licenseId) {
+        return String.format("This is the delete");
     }
 }
